1d6f648a1a11bc133f1c335c2f1fda99aca1561a
jitu-jk <jitesh62684@gmail.com>
2024-02-29 22:07:59 +0530

    changes

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,7 +1,21 @@
  const express=require('express');
  const router =express.Router();
  const User = require('../models/User');
- router.post("/CreateUser",async(req,res)=>{
+ const { body, validationResult } = require('express-validator');
+
+ router.post("/CreateUser",[
+ 
+ body('name').isLength({ min: 5 }),
+ body('password','incorrect password').isLength({ min: 5 }),
+ body('email').isEmail(),],
+ async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+        
+
     try{
         await User.create({
             name:"jitesh",
--- backend/models/User.js
+++ backend/models/User.js
@@ -5,15 +5,17 @@ const UserSchema = new Schema({
         type:String,
         required:true
     },
-    location:{
+    password:{
         type:String,
         required:true
     },
+  
     email:{
         type:String,
         required:true
     },
-    password:{
+ 
+    location:{
         type:String,
         required:true
     },
