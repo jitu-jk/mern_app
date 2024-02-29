a601663f11aac1cfc513e4ee9cb166048a0b65bc
jitu-jk <jitesh62684@gmail.com>
2024-02-29 22:52:04 +0530

    29/02/24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
            < Link className="nav-link" to="/login">Login</Link>
            </li>
            <li className="nav-item">
-           < Link className="nav-link" to="/creatuser">SignUp</Link>
+           < Link className="nav-link" to="/createuser">SignUp</Link>
             </li>
 
           </ul>
